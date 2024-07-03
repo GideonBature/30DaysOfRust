8e0ab50201066738b43ee4e9f1ff33213c863709
GideonBature <infoaboutgideon@gmail.com>
2024-07-03 23:33:36 +0100

    add code example

---
+++ day4/code11/src/main.rs
@@ -0,0 +1,22 @@
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+impl Rectangle {
+    fn width(&self) -> bool {
+        self.width > 0
+    }
+}
+
+fn main() {
+    let rect1 = Rectangle {
+        width: 30,
+        height: 50,
+    };
+
+    if rect1.width() {
+        println!("The rectangle has a nonzero width; it is {}", rect1.width);
+    }
+}
---
+++ day4/code11/target/debug/code11.d
@@ -0,0 +1,1 @@
+/home/bene/Desktop/30DaysOfRust/day4/code11/target/debug/code11: /home/bene/Desktop/30DaysOfRust/day4/code11/src/main.rs
