9d1de0e50c6b472e2d330461ade8d47ccb543cac
GideonBature <infoaboutgideon@gmail.com>
2024-07-03 23:52:57 +0100

    add code example

---
+++ day4/code11/target/debug/deps/code11-4265922d80db898d.d
@@ -0,0 +1,5 @@
+/home/bene/Desktop/30DaysOfRust/day4/code11/target/debug/deps/libcode11-4265922d80db898d.rmeta: src/main.rs
+
+/home/bene/Desktop/30DaysOfRust/day4/code11/target/debug/deps/code11-4265922d80db898d.d: src/main.rs
+
+src/main.rs:
