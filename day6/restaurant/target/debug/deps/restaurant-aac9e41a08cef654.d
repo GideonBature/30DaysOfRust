8776657137ed1dfa4363af7ba11398408714c818
GideonBature <infoaboutgideon@gmail.com>
2024-07-07 02:41:10 +0100

    paths for referring to an item in a module tree

---
+++ day6/restaurant/target/debug/deps/restaurant-aac9e41a08cef654.d
@@ -0,0 +1,5 @@
+/home/bene/Desktop/30DaysOfRust/day6/restaurant/target/debug/deps/librestaurant-aac9e41a08cef654.rmeta: src/lib.rs
+
+/home/bene/Desktop/30DaysOfRust/day6/restaurant/target/debug/deps/restaurant-aac9e41a08cef654.d: src/lib.rs
+
+src/lib.rs:
---
+++ day6/restaurant/target/debug/deps/restaurant-ed851233e533bb94.d
@@ -0,0 +1,7 @@
+/home/bene/Desktop/30DaysOfRust/day6/restaurant/target/debug/deps/librestaurant-ed851233e533bb94.rmeta: src/lib.rs
+
+/home/bene/Desktop/30DaysOfRust/day6/restaurant/target/debug/deps/librestaurant-ed851233e533bb94.rlib: src/lib.rs
+
+/home/bene/Desktop/30DaysOfRust/day6/restaurant/target/debug/deps/restaurant-ed851233e533bb94.d: src/lib.rs
+
+src/lib.rs:
