fff02526f503ec85f35525c22c67231cb6140e65
GideonBature <infoaboutgideon@gmail.com>
2024-07-11 12:14:10 +0100

    file handling error using unwrap() method

---
+++ day11/code7/src/main.rs
@@ -0,0 +1,5 @@
+use std::fs::File;
+
+fn main() {
+    File::open("hello.txt").unwrap();
+}
---
+++ day11/code7/target/debug/code7.d
@@ -0,0 +1,1 @@
+/home/bene/Desktop/30DaysOfRust/day11/code7/target/debug/code7: /home/bene/Desktop/30DaysOfRust/day11/code7/src/main.rs
