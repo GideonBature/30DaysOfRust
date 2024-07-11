62fdd4d728a7e96b143add95f129c1d9bc5c7915
GideonBature <infoaboutgideon@gmail.com>
2024-07-11 12:05:39 +0100

    file handling error using closures

---
+++ day11/code6/target/debug/code6.d
@@ -0,0 +1,1 @@
+/home/bene/Desktop/30DaysOfRust/day11/code6/target/debug/code6: /home/bene/Desktop/30DaysOfRust/day11/code6/src/main.rs
---
+++ day11/code6/target/debug/deps/code6-12b4f9acfb1b4e4d.d
@@ -0,0 +1,5 @@
+/home/bene/Desktop/30DaysOfRust/day11/code6/target/debug/deps/libcode6-12b4f9acfb1b4e4d.rmeta: src/main.rs
+
+/home/bene/Desktop/30DaysOfRust/day11/code6/target/debug/deps/code6-12b4f9acfb1b4e4d.d: src/main.rs
+
+src/main.rs:
