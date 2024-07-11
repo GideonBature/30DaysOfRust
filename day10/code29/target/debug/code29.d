289450339b3728d58bb7e30c60d55086d259f02e
GideonBature <infoaboutgideon@gmail.com>
2024-07-11 09:46:21 +0100

    update the codes

---
+++ day10/code29/target/debug/code29.d
@@ -0,0 +1,1 @@
+/home/bene/Desktop/30DaysOfRust/day10/code29/target/debug/code29: /home/bene/Desktop/30DaysOfRust/day10/code29/src/main.rs
---
+++ day10/code29/target/debug/deps/code29-7304c939f157208e.d
@@ -0,0 +1,5 @@
+/home/bene/Desktop/30DaysOfRust/day10/code29/target/debug/deps/code29-7304c939f157208e: src/main.rs
+
+/home/bene/Desktop/30DaysOfRust/day10/code29/target/debug/deps/code29-7304c939f157208e.d: src/main.rs
+
+src/main.rs:
