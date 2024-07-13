eec69e291354a40df1aff73c032d822d053b5654
GideonBature <infoaboutgideon@gmail.com>
2024-07-13 14:16:35 +0100

    experimenting with traits having a default method and overriding that method

---
+++ day12/code18/target/debug/deps/code14-4c6cbe38a03dfa92.d
@@ -0,0 +1,7 @@
+/home/bene/Desktop/30DaysOfRust/day12/code17/target/debug/deps/libcode14-4c6cbe38a03dfa92.rmeta: src/lib.rs
+
+/home/bene/Desktop/30DaysOfRust/day12/code17/target/debug/deps/libcode14-4c6cbe38a03dfa92.rlib: src/lib.rs
+
+/home/bene/Desktop/30DaysOfRust/day12/code17/target/debug/deps/code14-4c6cbe38a03dfa92.d: src/lib.rs
+
+src/lib.rs:
