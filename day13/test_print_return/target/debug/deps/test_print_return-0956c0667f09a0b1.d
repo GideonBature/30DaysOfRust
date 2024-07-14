656c07c4ab7296d665eed8e9d4995e8627abde75
GideonBature <infoaboutgideon@gmail.com>
2024-07-14 04:51:44 +0100

    testing using assert! method for text

---
+++ day13/test_print_return/target/debug/deps/test_print_return-0956c0667f09a0b1.d
@@ -0,0 +1,5 @@
+/home/bene/Desktop/30DaysOfRust/day13/test_print_return/target/debug/deps/test_print_return-0956c0667f09a0b1: src/lib.rs
+
+/home/bene/Desktop/30DaysOfRust/day13/test_print_return/target/debug/deps/test_print_return-0956c0667f09a0b1.d: src/lib.rs
+
+src/lib.rs:
---
+++ day13/test_print_return/target/debug/deps/test_print_return-95eb236f066e9bf6.d
@@ -0,0 +1,7 @@
+/home/bene/Desktop/30DaysOfRust/day13/test_print_return/target/debug/deps/libtest_print_return-95eb236f066e9bf6.rmeta: src/lib.rs
+
+/home/bene/Desktop/30DaysOfRust/day13/test_print_return/target/debug/deps/libtest_print_return-95eb236f066e9bf6.rlib: src/lib.rs
+
+/home/bene/Desktop/30DaysOfRust/day13/test_print_return/target/debug/deps/test_print_return-95eb236f066e9bf6.d: src/lib.rs
+
+src/lib.rs:
