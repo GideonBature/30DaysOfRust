5b7292c3e1f6be8b1d212a3500e388a4ae0997b6
GideonBature <infoaboutgideon@gmail.com>
2024-07-19 15:27:16 +0100

    code sample

---
+++ day14/iterator_test/target/debug/deps/iterator_test-2759d426200818a7.d
@@ -0,0 +1,7 @@
+/home/bene/Desktop/30DaysOfRust/day14/iterator_test/target/debug/deps/libiterator_test-2759d426200818a7.rmeta: src/lib.rs
+
+/home/bene/Desktop/30DaysOfRust/day14/iterator_test/target/debug/deps/libiterator_test-2759d426200818a7.rlib: src/lib.rs
+
+/home/bene/Desktop/30DaysOfRust/day14/iterator_test/target/debug/deps/iterator_test-2759d426200818a7.d: src/lib.rs
+
+src/lib.rs:
---
+++ day14/iterator_test/target/debug/deps/iterator_test-659f645aaad39921.d
@@ -0,0 +1,5 @@
+/home/bene/Desktop/30DaysOfRust/day14/iterator_test/target/debug/deps/iterator_test-659f645aaad39921: src/lib.rs
+
+/home/bene/Desktop/30DaysOfRust/day14/iterator_test/target/debug/deps/iterator_test-659f645aaad39921.d: src/lib.rs
+
+src/lib.rs:
