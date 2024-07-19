26192c5a2fb7a41e7e98363b6048407527949e84
GideonBature <infoaboutgideon@gmail.com>
2024-07-19 14:37:22 +0100

    code sample

---
+++ day14/mut_ref_closure/target/debug/deps/mut_ref_closure-67c1a63e2ee805a0.d
@@ -0,0 +1,5 @@
+/home/bene/Desktop/30DaysOfRust/day14/mut_ref_closure/target/debug/deps/mut_ref_closure-67c1a63e2ee805a0: src/main.rs
+
+/home/bene/Desktop/30DaysOfRust/day14/mut_ref_closure/target/debug/deps/mut_ref_closure-67c1a63e2ee805a0.d: src/main.rs
+
+src/main.rs:
---
+++ day14/mut_ref_closure/target/debug/mut_ref_closure.d
@@ -0,0 +1,1 @@
+/home/bene/Desktop/30DaysOfRust/day14/mut_ref_closure/target/debug/mut_ref_closure: /home/bene/Desktop/30DaysOfRust/day14/mut_ref_closure/src/main.rs
