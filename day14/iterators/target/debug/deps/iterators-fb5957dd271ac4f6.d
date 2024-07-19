9f010297d27d9daf542796119e92f2055640606c
GideonBature <infoaboutgideon@gmail.com>
2024-07-19 15:14:59 +0100

    code sample

---
+++ day14/iterators/target/debug/deps/iterators-fb5957dd271ac4f6.d
@@ -0,0 +1,5 @@
+/home/bene/Desktop/30DaysOfRust/day14/iterators/target/debug/deps/iterators-fb5957dd271ac4f6: src/main.rs
+
+/home/bene/Desktop/30DaysOfRust/day14/iterators/target/debug/deps/iterators-fb5957dd271ac4f6.d: src/main.rs
+
+src/main.rs:
---
+++ day14/iterators/target/debug/iterators.d
@@ -0,0 +1,1 @@
+/home/bene/Desktop/30DaysOfRust/day14/iterators/target/debug/iterators: /home/bene/Desktop/30DaysOfRust/day14/iterators/src/main.rs
