2f5d691027c426fe8b922dd660811868c7559aae
GideonBature <infoaboutgideon@gmail.com>
2024-07-21 13:28:01 +0100

    add one function

---
+++ day15/add/target/debug/adder.d
@@ -0,0 +1,1 @@
+/home/bene/Desktop/30DaysOfRust/day15/add/target/debug/adder: /home/bene/Desktop/30DaysOfRust/day15/add/adder/src/main.rs
---
+++ day15/add/target/debug/deps/adder-7d36d50579ac6a18.d
@@ -0,0 +1,5 @@
+/home/bene/Desktop/30DaysOfRust/day15/add/target/debug/deps/adder-7d36d50579ac6a18: adder/src/main.rs
+
+/home/bene/Desktop/30DaysOfRust/day15/add/target/debug/deps/adder-7d36d50579ac6a18.d: adder/src/main.rs
+
+adder/src/main.rs:
