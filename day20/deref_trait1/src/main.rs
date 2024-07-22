0e150ae442e526c765885798107da5d3b41f3a7c
GideonBature <infoaboutgideon@gmail.com>
2024-07-22 12:58:27 +0100

    smart pointers like reg pointers

---
+++ day20/deref_trait1/src/main.rs
@@ -0,0 +1,26 @@
+use std::ops::Deref;
+
+impl<T> Deref for MyBox<T> {
+    type Target = T;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+struct MyBox<T>(T);
+
+impl<T> MyBox<T> {
+    fn new(x: T) -> MyBox<T> {
+        MyBox(x)
+    }
+}
+
+fn hello(name: &str) {
+    println!("Hello, {name}!");
+}
+
+fn main() {
+    let m = MyBox::new(String::from("Rust"));
+    hello(&m);
+}
---
+++ day20/deref_trait1/target/debug/deref_trait.d
@@ -0,0 +1,1 @@
+/home/bene/Desktop/30DaysOfRust/day20/deref_trait1/target/debug/deref_trait: /home/bene/Desktop/30DaysOfRust/day20/deref_trait1/src/main.rs
