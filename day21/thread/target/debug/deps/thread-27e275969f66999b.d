645594da95cc24ed7eb23b5bb6d23ebc1ddf4f23
GideonBature <infoaboutgideon@gmail.com>
2024-07-24 00:44:33 +0100

    spawn thread with the join method

---
+++ day21/thread/target/debug/deps/thread-27e275969f66999b.d
@@ -0,0 +1,5 @@
+/home/bene/Desktop/30DaysOfRust/day21/thread/target/debug/deps/libthread-27e275969f66999b.rmeta: src/main.rs
+
+/home/bene/Desktop/30DaysOfRust/day21/thread/target/debug/deps/thread-27e275969f66999b.d: src/main.rs
+
+src/main.rs:
