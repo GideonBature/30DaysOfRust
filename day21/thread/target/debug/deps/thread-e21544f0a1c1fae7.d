fef0464967e83949e5948867120f2305aa357ef8
GideonBature <infoaboutgideon@gmail.com>
2024-07-24 00:42:21 +0100

    spawn thread

---
+++ day21/thread/target/debug/deps/thread-e21544f0a1c1fae7.d
@@ -0,0 +1,5 @@
+/home/bene/Desktop/30DaysOfRust/day21/thread/target/debug/deps/thread-e21544f0a1c1fae7: src/main.rs
+
+/home/bene/Desktop/30DaysOfRust/day21/thread/target/debug/deps/thread-e21544f0a1c1fae7.d: src/main.rs
+
+src/main.rs:
---
+++ day21/thread/target/debug/thread.d
@@ -0,0 +1,1 @@
+/home/bene/Desktop/30DaysOfRust/day21/thread/target/debug/thread: /home/bene/Desktop/30DaysOfRust/day21/thread/src/main.rs
