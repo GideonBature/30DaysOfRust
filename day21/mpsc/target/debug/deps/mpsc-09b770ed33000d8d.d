efc2366d3a80a5ba575ee3ecc83c82d3ae3c0ab3
GideonBature <infoaboutgideon@gmail.com>
2024-07-24 13:46:25 +0100

    spawn thread with mpsc

---
+++ day21/mpsc/target/debug/deps/mpsc-09b770ed33000d8d.d
@@ -0,0 +1,5 @@
+/home/bene/Desktop/30DaysOfRust/day21/mpsc/target/debug/deps/mpsc-09b770ed33000d8d: src/main.rs
+
+/home/bene/Desktop/30DaysOfRust/day21/mpsc/target/debug/deps/mpsc-09b770ed33000d8d.d: src/main.rs
+
+src/main.rs:
---
+++ day21/mpsc/target/debug/mpsc.d
@@ -0,0 +1,1 @@
+/home/bene/Desktop/30DaysOfRust/day21/mpsc/target/debug/mpsc: /home/bene/Desktop/30DaysOfRust/day21/mpsc/src/main.rs
