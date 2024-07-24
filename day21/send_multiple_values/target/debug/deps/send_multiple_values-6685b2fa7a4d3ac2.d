219a543bce1eb3bee6105435de8f22513b936f6b
GideonBature <infoaboutgideon@gmail.com>
2024-07-24 13:58:28 +0100

    spawn thread with mpsc

---
+++ day21/send_multiple_values/target/debug/deps/send_multiple_values-6685b2fa7a4d3ac2.d
@@ -0,0 +1,5 @@
+/home/bene/Desktop/30DaysOfRust/day21/send_multiple_values/target/debug/deps/send_multiple_values-6685b2fa7a4d3ac2: src/main.rs
+
+/home/bene/Desktop/30DaysOfRust/day21/send_multiple_values/target/debug/deps/send_multiple_values-6685b2fa7a4d3ac2.d: src/main.rs
+
+src/main.rs:
---
+++ day21/send_multiple_values/target/debug/send_multiple_values.d
@@ -0,0 +1,1 @@
+/home/bene/Desktop/30DaysOfRust/day21/send_multiple_values/target/debug/send_multiple_values: /home/bene/Desktop/30DaysOfRust/day21/send_multiple_values/src/main.rs
