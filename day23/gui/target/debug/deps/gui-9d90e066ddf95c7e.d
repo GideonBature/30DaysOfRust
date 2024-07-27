c93e894e1181e67be299536cffe763cf633fc2dc
GideonBature <infoaboutgideon@gmail.com>
2024-07-27 01:51:27 +0100

    the Rust object oriented programming

---
+++ day23/gui/target/debug/deps/gui-9d90e066ddf95c7e.d
@@ -0,0 +1,5 @@
+/home/bene/Desktop/30DaysOfRust/day23/gui/target/debug/deps/libgui-9d90e066ddf95c7e.rmeta: src/lib.rs
+
+/home/bene/Desktop/30DaysOfRust/day23/gui/target/debug/deps/gui-9d90e066ddf95c7e.d: src/lib.rs
+
+src/lib.rs:
---
+++ day23/gui/target/debug/deps/gui-eac56391c9cdb13f.d
@@ -0,0 +1,5 @@
+/home/bene/Desktop/30DaysOfRust/day23/gui/target/debug/deps/libgui-eac56391c9cdb13f.rmeta: src/main.rs
+
+/home/bene/Desktop/30DaysOfRust/day23/gui/target/debug/deps/gui-eac56391c9cdb13f.d: src/main.rs
+
+src/main.rs:
